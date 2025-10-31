947713085c8b5eff501cfd3bd57a6dab25fe4efc
sarahwtz <sarahwtz@gmail.com>
2025-10-31 14:56:19 +0100

    feat: implement AbstractRepository as base for repositories

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace App\Repositories;
+use Illuminate\Database\Eloquent\Model;
+
+
+abstract class AbstractRepository {
+
+     protected $model;
+     public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function selectRelatedFields($fields)
+    {
+        $this->model = $this->model->with($fields);
+    }
+
+    public function filter($filters)
+    {
+       
+        $filters = explode(';', $filters);
+
+        foreach ($filters as $condition) {
+            $c = explode(':', $condition);
+            $this->model = $this->model->where($c[0], $c[1], $c[2]);
+        }
+    }
+
+    public function selectFields($fields)
+    {
+        $this->model = $this->model->selectRaw($fields);
+    }
+
+    public function getResult()
+    {
+        return $this->model->get();
+    }
+}
+
+
+
+?>
